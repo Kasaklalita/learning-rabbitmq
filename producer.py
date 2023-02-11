feba116a5b7057b8636225ec53b09ce9b3c07fe2
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-11 19:55:28 +0300

    Pubsub

--- producer.py
+++ producer.py
@@ -1,6 +1,7 @@
 import pika
 import time
 import random
+from pika.exchange_type import ExchangeType
 
 connection_parameters = pika.ConnectionParameters('localhost')
 
@@ -8,13 +9,20 @@ connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='letterbox')
+channel.exchange_declare(exchange='pubsub', exchange_type=ExchangeType.fanout)
 
-message_id = 1
+message = "hello I want to broadcast this message"
 
-while True:
-    message = f'Sending message_id: {message_id}'
-    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
-    print(f'Sent message: {message}')
-    time.sleep(random.randint(1, 4))
-    message_id += 1
+channel.basic_publish(exchange='pubsub', routing_key='', body=message)
+
+print(f'Sent message: {message}')
+time.sleep(random.randint(1, 4))
+
+# message_id = 1
+
+# while True:
+#     message = f'Sending message_id: {message_id}'
+#     channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+#     print(f'Sent message: {message}')
+#     time.sleep(random.randint(1, 4))
+#     message_id += 1
---
+++ secondconsumer.py
@@ -0,0 +1,36 @@
+import pika
+import time
+import random
+from pika.exchange_type import ExchangeType
+
+
+def on_message_received(ch, method, properties, body):
+    print(f'Second consumer: received new message: {body}')
+
+    # processing_time = random.randint(1, 6)
+    # print(f'First recieved: {body}, will take {processing_time} to process')
+    # time.sleep(processing_time)
+    # ch.basic_ack(delivery_tag=method.delivery_tag)
+    # print('Finished processing the message')
+
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='pubsub', exchange_type=ExchangeType.fanout)
+
+queue = channel.queue_declare(queue='', exclusive=True)
+
+channel.queue_bind(exchange='pubsub', queue=queue.method.queue)
+
+channel.basic_qos(prefetch_count=1)
+
+channel.basic_consume(
+    queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
+
+print('Started consuming')
+
+channel.start_consuming()
